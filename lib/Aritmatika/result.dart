2ebfed25b880b6f5fa2f23e721bcf2fe18fbf41d
AK7563 <nailzhavieradhyaksa441@gmail.com>
2024-09-03 16:23:55 +0700

    AddedArithmetik

---
+++ lib/Aritmatika/result.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/widgets.dart';
+import 'package:menghitung_bangun_ruang/Aritmatika/aritmatika.dart';
+
+class ResultDisplay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final state = ArithmeticState.of(context);
+    if (state == null) return SizedBox.shrink();
+
+    int result;
+    switch (state.operation) {
+      case '+':
+        result = state.number1 + state.number2;
+        break;
+      case '-':
+        result = state.number1 - state.number2;
+        break;
+      case '*':
+        result = state.number1 * state.number2;
+        break;
+      case '/':
+        result = state.number2 != 0 ? state.number1 ~/ state.number2 : 0;
+        break;
+      default:
+        result = 0;
+    }
+
+    return Text(
+      'Result: $result',
+      style: TextStyle(fontSize: 24),
+    );
+  }
+}
